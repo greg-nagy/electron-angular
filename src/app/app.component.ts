d40a978cb8be5e15763c08ec9cb7bd2d6718a0b8
puffy <nagy.gergo@3pixel.hu>
2018-02-07 13:21:35 +0100

    plain integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+// import { shell } from 'electron';
+// const { shell } = require('electron');
+// const { shell } = window.require('electron');
+const { shell } = (<any>window).require('electron');
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  open() {
+    shell.openExternal('http://netacademia.hu');
+  }
 }
